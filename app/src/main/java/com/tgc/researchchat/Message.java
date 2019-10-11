083f014d478fb79fc897a443d3de38e69fb67f58
istiakshihab <shihabistiak@gmail.com>
2019-10-12 01:50:43 +0600

    Refactor

--- app/src/main/java/com/tgc/researchchat/Message.java
+++ app/src/main/java/com/tgc/researchchat/Message.java
@@ -1,19 +1,19 @@
 package com.tgc.researchchat;
 
-public class Message {
+class Message {
     String message;
     int type;
-    public Message(String message,int type){
+    Message(String message, int type){
         this.message=message;
         this.type=type;
     }
-    public boolean isSent(){
+    boolean isSent(){
         if(type==0){
             return true;
         }
         return false;
     }
-    public String getMessage(){
+    String getMessage(){
         return message;
     }
 }

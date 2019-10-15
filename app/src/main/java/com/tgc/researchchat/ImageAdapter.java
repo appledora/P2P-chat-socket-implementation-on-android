cc369ee0497c13320213c476ddcfe27a6a85c373
appledora <embers1010@gmail.com>
2019-10-15 16:58:10 +0600

    Image Show

---
+++ app/src/main/java/com/tgc/researchchat/ImageAdapter.java
@@ -0,0 +1,63 @@
+package com.tgc.researchchat;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class ImageAdapter extends ArrayAdapter {
+
+    ImageAdapter(Activity context, ArrayList<MyFiles> arr) {
+        super(context, 0, arr);
+    }
+
+
+    @Override
+    public View getView(int position, View converView, ViewGroup parent) {
+        View listItemView = converView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.messagelist, parent, false);
+        }
+        MyFiles currentFile = (MyFiles) getItem(position);
+
+        assert currentFile != null;
+        ImageView sentImage = listItemView.findViewById(R.id.image_sent);
+        ImageView receivedImage = listItemView.findViewById(R.id.image_received);
+
+
+        System.out.println(currentFile.isSent());
+        String filePath = currentFile.getFilePath();
+        System.out.println("current file path =>" + filePath);
+
+
+        if (currentFile.isSent()) {
+            File imgFile = new File(filePath);
+            if (imgFile.exists()) {
+
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+                sentImage.setImageBitmap(myBitmap);
+                sentImage.setVisibility(View.VISIBLE);
+
+            }
+        } else {
+            File imgFile = new File(filePath);
+            System.out.println("filePath while received => " + filePath);
+            if (imgFile.exists()) {
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                receivedImage.setImageBitmap(myBitmap);
+                receivedImage.setVisibility(View.VISIBLE);
+            }
+        }
+
+        return listItemView;
+    }
+}
---
+++ app/src/main/java/com/tgc/researchchat/MyFiles.java
@@ -0,0 +1,18 @@
+package com.tgc.researchchat;
+
+public class MyFiles {
+    private String Path;
+    private int type;
+    MyFiles(String Path, int type){
+        this.Path=Path;
+        this.type=type;
+    }
+
+    boolean isSent(){
+        return type == 0;
+    }
+
+    String getFilePath(){
+        return Path;
+    }
+}
